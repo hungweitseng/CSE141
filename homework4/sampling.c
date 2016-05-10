0f8aa9dd61d966ba236b2a35704dfbc7b74f9634
Hung-Wei Tseng <bunnyhwtseng@bunny.ucsd.edu>
2016-05-09 21:29:37 -0700

    Updated sampling.c

--- homework4/sampling.c
+++ homework4/sampling.c
@@ -5,7 +5,7 @@
 int main()
 {
   int i,j,k,stride,samples=1024;
-  double *data, average;
+  double *data, average, execution_time;
   struct timeval time_start, time_end;
   data = (double *)malloc(sizeof(double)*32768*1024);
   for(i = 0 ; i < 32768*1024; i++)
@@ -13,13 +13,14 @@ int main()
   for(stride = 8; stride < 65536; stride*=2)
   {
     gettimeofday(&time_start, NULL);
-    for(i = 0; i < 100000; i++)
+    for(i = 0; i < 10000; i++)
     {
       for(j=0;j<samples;j++)
         average+=data[(j*stride)%(32768*1024)];
     }
     gettimeofday(&time_end, NULL);
-    fprintf(stderr, "Sampled %d numbers from every %d numbers: %lf seconds,  average = %lf\n",samples,stride,((time_end.tv_sec * 1000000 + time_end.tv_usec) - (time_start.tv_sec * 1000000 + time_start.tv_usec))/1000000.0, average);
+    execution_time = ((time_end.tv_sec * 1000000 + time_end.tv_usec) - (time_start.tv_sec * 1000000 + time_start.tv_usec))/1000000.0;
+    fprintf(stderr, "Distance between samples: %d items, throughput: %lf MB/seconds, average = %lf\n",stride, (double)(10000*samples)/(execution_time*1024*1024), average/samples);
   }
   return 0;
 }
